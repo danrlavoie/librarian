a63fb6c57faede2b5e4adc55cab7b4c238549627
Dan Lavoie <dan.r.lavoie+github@gmail.com>
2019-07-07 09:19:20 -0400

    create initial file for bot

---
+++ librarian.py
@@ -0,0 +1,14 @@
+import os
+import slack
+import logging
+import ssl as ssl_lib
+import certifi
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+    slack_token = os.environ["SLACK_BOT_TOKEN"]
+    rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
+    rtm_client.start()
